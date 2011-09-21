ab18229508bf27ac842efb8515f7787eb2d8bcbe
Pankaj <creativedesigner2k9@gmail.com>
2011-09-22 02:23:12 +0530

    bandprofile page added

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,6 +2,12 @@ $(function() {
 
 	$(".accordion").accordion({	header:'h2'});
 	
-	$('#sidebar .band .accordion div').attr({style:'height:194px;overflow:hidden;'})
+	$('#sidebar .band .accordion div').attr({style:'height:194px;overflow:hidden;'});
 	
+	$('#playlist li a').ellipsis();
+	
+	$('li:first-child').addClass('first');
+	$('li:last-child').addClass('last');
+	$('li:nth-child(2n)').addClass('even');
+	$('#band-details-table tr td:first-child').addClass('label');
 })
