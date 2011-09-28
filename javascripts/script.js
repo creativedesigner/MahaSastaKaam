897b9ceccc550aa94401ba964032487fc9233b5b
Pankaj <creativedesigner2k9@gmail.com>
2011-09-28 06:45:09 +0530

    seacrh page complete

--- javascripts/script.js
+++ javascripts/script.js
@@ -10,4 +10,27 @@ $(function() {
 	$('li:last-child').addClass('last');
 	$('li:nth-child(2n)').addClass('even');
 	$('tr td:first-child').addClass('label');
+	
+	$('#search-bar span.close').click(function(){
+		$('#search-bar p').css({visibility:'hidden'});
+	});
+	
+	$('#search-bar .panel input[type=radio][value=song]').attr('checked','checked');
+	
+	$('#search-filters li a').click(function(){
+		var text = $.trim($(this).text().toLowerCase());
+		$('#search-filters li a.active').removeClass('active');
+		$(this).addClass('active');
+		$('#search-bar .panel input[type=radio]:checked').removeAttr('checked');
+		
+		$('#search-bar .panel input[type=radio][value='+text+']').attr('checked','checked');
+		
+		
+		// toggle results
+		$('#search-results').toggle();
+		$('#search-results-wrapper .note').toggle();
+		
+	});
+	
+	
 })
